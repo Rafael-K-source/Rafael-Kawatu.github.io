13e2de0f759fe93ee9d4988507c4aee045a1cd8e
Rafa_15 <kawaturafael@gmail.com>
2026-05-10 20:38:52 +0800

    Update script.js

--- css/js/script.js
+++ css/js/script.js
@@ -1,7 +1,13 @@
-function darkMode() {
-    document.body.classList.toggle("dark");
+const text = "Welcome to Rafael Portfolio";
+let index = 0;
+
+function typingEffect() {
+    if (index < text.length) {
+        document.title += text.charAt(index);
+        index++;
+        setTimeout(typingEffect, 100);
+    }
 }
 
-function kirimPesan() {
-    alert("Pesan berhasil dikirim!");
-}
+document.title = "";
+typingEffect();
